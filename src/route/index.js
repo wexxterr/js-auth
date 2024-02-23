f634a03c86c6b74030c273742b658a330f45a1bd
wexxterr <bilousvladw@gmail.com>
2024-02-23 08:36:24 +0100

    init

--- src/route/index.js
+++ src/route/index.js
@@ -2,12 +2,25 @@
 const express = require('express')
 const router = express.Router()
 
+router.get('/', function (req, res) {
+
+    res.render('index', {
+        name: 'index', 
+
+        component: [],
+
+        title: 'Home page',
+
+        data:{},
+    })
+})
+
 // Підключіть файли роутів
-const test = require('./test')
+const test = require('./auth')
 // Підключіть інші файли роутів, якщо є
 
 // Об'єднайте файли роутів за потреби
-router.use('/', test)
+router.use('/', auth)
 // Використовуйте інші файли роутів, якщо є
 
 // Експортуємо глобальний роутер
